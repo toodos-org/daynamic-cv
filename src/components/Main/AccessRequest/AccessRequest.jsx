261dc3481563d0725d2c795c48ffc200bc08e6cf
saikatnoman12 <noommaa240@gmail.com>
2024-02-01 01:18:54 +0600

    get form data.

--- src/components/Main/AccessRequest/AccessRequest.jsx
+++ src/components/Main/AccessRequest/AccessRequest.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
 import "./AccessRequest.css";
 
 function AccessRequest() {
+  // access request get input data
+  const [accessReq, getAccessReq] = useState("");
+
+  // access request input err msg show
+  const [accessReqErr, setAccessReqErr] = useState(false);
+
+  /**
+   * access req form handle function
+   * accessReqFormHandle()
+   */
+
+  function accessReqFormHandle(e) {
+    if (accessReq == "" && accessReq.trim() == "") {
+      setAccessReqErr(true);
+    } else {
+      const regex = /\S+@\S+\.\S+/;
+      if (regex.test(accessReq)) {
+        console.log("your mail is valid");
+
+        setAccessReqErr(false);
+      } else {
+        setAccessReqErr(true);
+      }
+    }
+    e.preventDefault();
+  }
+
   return (
     <>
       <section className="access-req">
@@ -11,14 +39,24 @@ function AccessRequest() {
           {/* --- END! access-req-main-left --- */}
 
           <div className="access-req-main-right">
-            <form>
+            <form onSubmit={(e) => accessReqFormHandle(e)}>
               <div className="input-box">
                 <p>
                   <label htmlFor="email">Email</label>
                 </p>
 
                 <div className="input-box-area">
-                  <input id="email" type="text" placeholder="Enter email" />
+                  <input
+                    id="email"
+                    type="text"
+                    placeholder="Enter email"
+                    onChange={(e) => getAccessReq(e.target.value)}
+                  />
+                  {accessReqErr ? (
+                    <p>
+                      <span>This fill is required!</span>
+                    </p>
+                  ) : null}
                 </div>
               </div>
               {/* --- END! input-box (email) --- */}
